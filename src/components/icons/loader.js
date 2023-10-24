9a65849b1ac20c7c8c1db66d6549b08f8cac1a65
Krunal Shambharkar <129111636+krunalss@users.noreply.github.com>
2023-10-25 01:52:31 +0530

    Update loader.js
    removed path 
    added path wu=ith letter k

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -10,19 +10,10 @@ const IconLoader = () => (
           fill="currentColor"
         />
       </g>
-      <path
-        stroke="currentColor"
-        strokeWidth="5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M 50, 5
-                  L 11, 27
-                  L 11, 72
-                  L 50, 95
-                  L 89, 73
-                  L 89, 28 z"
-      />
     </g>
+    <text x="28" y="65" fill="currentColor" fontSize="50px" fontFamily="Consolas, serif">
+      K
+    </text>
   </svg>
 );
 
