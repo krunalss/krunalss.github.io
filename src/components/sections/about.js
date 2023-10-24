e1fd7c17d2c36c9a80c1032470790342ce9a7603
Krunal Shambharkar <129111636+krunalss@users.noreply.github.com>
2023-10-25 02:21:48 +0530

    Update about.js
    wrote my own "about me"

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -125,7 +125,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Git & DVC (for versioning)', 'AWS SageMaker', 'TFX (TensorFlow Extended)', 'GitHub Actions', 'MLFlow', 'Dagshub'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +135,19 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Krunal and I thrive on bringing Machine Learning models from the lab to production. My journey into MLOps began in 2018 when I stumbled upon the intricacies of deploying a simple linear regression model into a real-world application — who knew orchestrating a model deployment pipeline could teach me so much about scalability and efficiency!
             </p>
 
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              <a href="https://digitate.com/">software venture</a>,
+              and{' '}
+              <a href="https://www.asml.com/en">supplier of photolithography equipment used in the semiconductor manufacturing industry</a>.
+                Currently, I'm dedicating my time to deepening my expertise and exploring the evolving landscape of MLOps.
             </p>
 
             <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              On the side, I also mentor enthusiasts on how to navigate the MLOps landscape using tools like MLflow and AWS SageMaker.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
